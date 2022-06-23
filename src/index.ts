5951e7b2a7132f46ca8c648c23e570d26ef06e5d
k.jeong <kwanwoo.jeong@gmail.com>
2022-06-24 01:10:44 +0900

    release version

--- src/index.ts
+++ src/index.ts
@@ -13,22 +13,43 @@ const VERSION = '0.0.1';
     const projectVersions = await jiraClient.projectVersions.getProjectVersions({
       projectIdOrKey: PROJECT_KEY,
     });
-    const existingVersion = projectVersions.find((v) => v.name === versionName);
-    if (!existingVersion) {
-      await jiraClient.projectVersions.createVersion({ name: versionName, project: PROJECT_KEY });
+    let projectVersion =
+      projectVersions.find((v) => v.name === versionName) ||
+      (await jiraClient.projectVersions.createVersion({
+        name: versionName,
+        project: PROJECT_KEY,
+      }));
+
+    if (!projectVersion) {
+      setFailed('Project Version not found');
+      return;
     }
 
-    await jiraClient.issues.editIssue({
-      issueIdOrKey: ISSUE_KEYS[0],
-      fields: {
-        fixVersions: [{ name: versionName }],
-      },
+    await Promise.all(
+      ISSUE_KEYS.map(async (issueKey) => {
+        const transitions = await jiraClient.issues.getTransitions({ issueIdOrKey: issueKey });
+        const doneTransition = transitions.transitions?.find((t) => t.name === 'Done');
+        if (!doneTransition) {
+          return;
+        }
+        return await Promise.all([
+          await jiraClient.issues.doTransition({
+            issueIdOrKey: issueKey,
+            transition: { id: doneTransition.id },
+          }),
+          await jiraClient.issues.editIssue({
+            issueIdOrKey: issueKey,
+            fields: { fixVersions: [{ name: versionName }] },
+          }),
+        ]);
+      }),
+    );
+
+    await jiraClient.projectVersions.updateVersion({
+      id: projectVersion.id!,
+      project: PROJECT_KEY,
+      released: true,
     });
-    // await Promise.all(
-    //   ISSUE_KEYS.map(
-    //     async (issueKey) =>
-    //   ),
-    // );
   } catch (error: any) {
     console.table(error);
     setFailed(error?.errorMessages);
