20d579f88b7549700b304f232f6799dfa0ae18f7
k.jeong <kwanwoo.jeong@gmail.com>
2022-06-24 00:17:32 +0900

    create version test

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ import { initJiraClient } from './jira';
 
 (async () => {
   const jiraClient = initJiraClient();
-  const issue = await jiraClient.issues.getIssue({ issueIdOrKey: 'ACTION-1' });
-  console.log(issue);
+  await jiraClient.projectVersions.createVersion({ name: 'v0.0.1', project: 'ACTION' });
 })();
